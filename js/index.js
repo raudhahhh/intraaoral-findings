20cdab6b5d327dcf400e756eca62c352911dc1e2
Ainul Raudhah Nor Hakma <126445992+raudhahhh@users.noreply.github.com>
2026-01-20 22:21:47 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ if (startBtn) {
     // CLEAR EVERYTHING
     localStorage.removeItem("patientData");
     localStorage.removeItem("bpeData");
+    localStorage.removeItem("rdesData");
 
     // START FRESH
     window.location.href = "oral-status.html";
